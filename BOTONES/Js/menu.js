7d2c371b47184c7f356421858a7beccbbce46618
fccom07 <fccom07@hotmail.com>
2020-09-24 13:53:58 -0400

    vol5

---
+++ BOTONES/Js/menu.js
@@ -0,0 +1,9 @@
+function MenuDesplegable() {
+	document.getElementsByClassName
+	("navigation")[0].classList.toggle("responsive");
+}
+
+function MenuDesplegable2() {
+	document.getElementsByClassName
+	("navigation")[1].classList.toggle("responsive");
+}
